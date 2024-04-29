75205471aaa904d7497c406899730190c064f750
Henrique Luís Oliveira Marques <henrique.marques@caf.io>
2024-04-29 09:11:31 -0300

    chore: pull request

---
+++ .github/pull_request_template.md
@@ -0,0 +1,14 @@
+### Summary
+<!-- Brief description of WHAT you’re doing and WHY. -->
+
+### JIRA ticket
+Link: [SDK-xx](https://caf.atlassian.net/browse/SDK-xx)
+
+### My solution
+<!-- Brief description of your solution -->
+
+### Impact analysis
+
+### Relevant info and/or screenshots
+
+cc 
---
+++ .github/workflows/scripts/parse_swiftlint_output.py
@@ -0,0 +1,32 @@
+import re
+
+def parse_swiftlint_output():
+    try:
+        # Read the output from SwiftLint
+        with open("swiftlint_output.txt", "r") as f:
+            output = f.read()
+    except:
+        print("The file 'swiftlint_output.txt' could not be found.")
+        exit(1)
+
+    # Find the summary message at the end of the output
+    match = re.search(r"Done linting! Found (\d+) violations, (\d+) serious in (\d+) files", output)
+
+    # Extract values from the summary message
+    violations = int(match.group(1))
+    serious_violations = int(match.group(2))
+    files = int(match.group(3))
+    warnings = violations - serious_violations
+
+    # Save the parsed information for later use
+    with open("swiftlint_summary.txt", "w") as summary:
+        if serious_violations == 0:
+            summary.write(f"✅ SwiftLint Succeeded!\n\n")
+        else:
+            summary.write(f"🛑 SwiftLint Failed!\n\n")
+            summary.write(f"⚠️ Warnings: {warnings}\n")
+            summary.write(f"🚨 Errors: {serious_violations}\n")
+            summary.write(f"📂 Files validated: {files}\n")
+
+if __name__ == "__main__":
+    parse_swiftlint_output()
