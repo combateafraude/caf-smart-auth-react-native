79b8641b06c5cc9e3aaa9d530035f9936a7278f0
Henrique Luís Oliveira Marques <henrique.marques@caf.io>
2024-04-29 09:33:37 -0300

    chore: test

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Button, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { IIdentityConfig, IdentityCAFStage, IdentityFilter } from './src/caf-bridge-sdk/IdentityHook/types.d';
+import {
+  IIdentityConfig,
+  IdentityCAFStage,
+  IdentityFilter,
+} from './src/caf-bridge-sdk/IdentityHook/types.d';
 import IdentityHook from './src/caf-bridge-sdk/IdentityHook';
 
-const App: React.FC<any> = () => {
-
+const App: React.FC<React.FC> = () => {
   const backgroundStyle = {
     backgroundColor: Colors.lighter,
   };
 
-  //Insert your generated JWT here. Check documentation here: https://docs.caf.io/sdks/access-token
-  const mobileToken = "";
-  const identityToken = ""
-  
-  const CPF = ""
+  // Insert your generated JWT here. Check documentation here: https://docs.caf.io/sdks/access-token
+  const mobileToken = '';
+  const identityToken = '';
+
+  const CPF = '';
 
   const IdentityConfig: IIdentityConfig = {
     cafStage: IdentityCAFStage.DEV,
@@ -23,57 +34,93 @@ const App: React.FC<any> = () => {
     livenessToken: mobileToken,
     setEnableScreenshots: false,
     setLoadingScreen: false,
-    filter: IdentityFilter.LINE_DRAWING
-  }
+    filter: IdentityFilter.LINE_DRAWING,
+  };
 
   // Identity
-  const [sendIdentity, identityData, loadingIdentity, identityError] = IdentityHook(identityToken, "policy", IdentityConfig);
+  const [sendIdentity, identityData, loadingIdentity, identityError] =
+    IdentityHook(identityToken, 'policy', IdentityConfig);
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle='dark-content'
+        barStyle="dark-content"
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+        style={backgroundStyle}
+      >
         <View
           style={{
             backgroundColor: Colors.white,
-          }}>
+          }}
+        >
           <View style={styles.sectionContainer}>
             <Button
-              title='Execute Identity'
+              title="Execute Identity"
               onPress={() => sendIdentity(CPF)}
             />
-            <Text style={[styles.sectionTitle, {
-              color: Colors.black
-            },]}>
-              {loadingIdentity ? 'Loading ...' : ''}</Text>
-            <Text style={[styles.sectionTitle, {
-              color: Colors.black
-            },]}>
-              {userCanceled ? 'User canceled the action' : ''}</Text>
+            <Text
+              style={[
+                styles.sectionTitle,
+                {
+                  color: Colors.black,
+                },
+              ]}
+            >
+              {loadingIdentity ? 'Loading ...' : ''}
+            </Text>
+            <Text
+              style={[
+                styles.sectionTitle,
+                {
+                  color: Colors.black,
+                },
+              ]}
+            >
+              {/* {userCanceled ? 'User canceled the action' : ''} */}
+            </Text>
             <View style={styles.sectionResultContainer}>
-              {!!identityError && <Text style={[styles.sectionResultText, {
-                color: Colors.black
-              },]}>
-                {`${identityError.type}: \n ${identityError.message}`}</Text>}
-              {identityData && <Text style={[styles.sectionResultText, {
-                color: Colors.black
-              },]}>
-                {'IdentityResult:\n' + JSON.stringify(identityData)}</Text>}
+              {!!identityError && (
+                <Text
+                  style={[
+                    styles.sectionResultText,
+                    {
+                      color: Colors.black,
+                    },
+                  ]}
+                >
+                  {`${identityError.type}: \n ${identityError.message}`}
+                </Text>
+              )}
+              {identityData && (
+                <Text
+                  style={[
+                    styles.sectionResultText,
+                    {
+                      color: Colors.black,
+                    },
+                  ]}
+                >
+                  {'IdentityResult:\n' + JSON.stringify(identityData)}
+                </Text>
+              )}
             </View>
-            <Text style={[styles.sectionTitle, {
-              color: Colors.black
-            },]}></Text>
+            <Text
+              style={[
+                styles.sectionTitle,
+                {
+                  color: Colors.black,
+                },
+              ]}
+            ></Text>
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   sectionContainer: {
--- src/caf-bridge-sdk/IdentityHook/types.d.ts
+++ src/caf-bridge-sdk/IdentityHook/types.d.ts
@@ -1,50 +1,51 @@
-import { FaceAuthenticatorFilter } from "../FaceAuthenticatorHook/types.d";
+import { FaceAuthenticatorFilter } from '../FaceAuthenticatorHook/types.d';
 
 export type IdentityErrorType = {
-    type: string;
-    message: string;
+  type: string;
+  message: string;
+};
+
+export interface IIdentityResponse {
+  authorized?: boolean;
+  pending?: boolean;
+  attestation?: string;
+  attemptId?: string;
 }
 
-export type IdentitySDKResponseType = Partial <IdentityErrorType> & IIdentityResponse;
+export type IdentitySDKResponseType = Partial<IdentityErrorType> &
+  IIdentityResponse;
 
 export type IdentityEvent =
-    | "Identity_Success"
-    | "Identity_Pending"
-    | "Identity_Error"
-    | "Identity_Canceled";
+  | 'Identity_Success'
+  | 'Identity_Pending'
+  | 'Identity_Error'
+  | 'Identity_Canceled';
 
 export type IdentityHookReturnType = [
-    (token: string) => void,
-    IdentityResponseType | undefined,
-    boolean,
-    IdentityErrorType | undefined,
-    boolean
+  (token: string) => void,
+  IdentityResponseType | undefined,
+  boolean,
+  IdentityErrorType | undefined,
+  boolean,
 ];
 
 export enum IdentityCAFStage {
-    BETA,
-    PROD,
-    DEV,
+  BETA,
+  PROD,
+  DEV,
 }
 
 export enum IdentityFilter {
-    LINE_DRAWING,
-    NATURAL
-}
-
-export interface IIdentityResponse {
-    authorized?: boolean;
-    pending?: boolean;
-    attestation?: string;
-    attemptId?: string;
+  LINE_DRAWING,
+  NATURAL,
 }
 
 export interface IIdentityConfig {
-    livenessToken: string | null;
-    setEnableScreenshots: boolean;
-    setLoadingScreen: boolean;
-    cafStage: CAFStage;
-    setEmailUrl: string | null;
-    setPhoneUrl: string | null;
-    filter: FaceAuthenticatorFilter
+  livenessToken: string | null;
+  setEnableScreenshots: boolean;
+  setLoadingScreen: boolean;
+  cafStage: CAFStage;
+  setEmailUrl: string | null;
+  setPhoneUrl: string | null;
+  filter: FaceAuthenticatorFilter;
 }
